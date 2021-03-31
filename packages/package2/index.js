2c40ef32ecf42073f53c12c1e636aa79b8333cba
Yaron Bar <rugggger@yahoo.co.uk>
2021-03-31 15:26:20 +0300

    second

---
+++ packages/package2/index.js
@@ -0,0 +1,7 @@
+function myFunc(){
+    console.log('package2Func');
+}
+
+module.exports = {
+    package2Func: myFunc
+}
--- packages/server/index.js
+++ packages/server/index.js
@@ -1,3 +1,5 @@
 const commonFunction = require("@walnut/common");
+const package2Func = require("@walnut/package2")
 
 commonFunction();
+package2Func.package2Func();
